3b992842a3cc91c3509c5d14829700329ca7d401
ljaeger <ljaeger@mtech.edu>
2024-01-26 12:54:29 -0700

    Initial commit

---
+++ RSA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+namespace RSA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 199, q = 167;
+            int n = p * q;
+
+
+        }
+        
+        static void Encode(char plain, int key)
+        {
+            
+        }
+        
+        static void EncodeMessage(string plain, int key)
+        {
+            
+        }
+
+        static void DecodeMessage(char cipher, int key)
+        {
+            
+        }
+    }
+}
